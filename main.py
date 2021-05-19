17acc4293d306640d8c4ef33305df755d27735ba
Harrison Verhine <>
2021-05-19 19:49:21 +0000

    Added a "base.html" template with a nav bar. Removed example "/page" page.

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ from random import choice
 
 web_site = Flask(__name__)
 
+#Loads in database from JSON
+'''
 for k in db.keys():
   del db[k]
 with open('./sampledatabase.json') as f:
   data = json.load(f)
 for k, v in data.items():
   db[k] = v
+'''
 
 def recurse_prim(x):
   x = database.to_primitive(x)
@@ -23,12 +26,6 @@ def recurse_prim(x):
 
 data = {k:recurse_prim(v) for k,v in db.items()}
 
-
-number_list = [
-	100, 101, 200, 201, 202, 204, 206, 207, 300, 301, 302, 303, 304, 305, 307, 400, 401, 402, 403, 404, 405, 406, 408, 409, 410, 411, 412, 413, 414, 415,
-	416, 417, 418, 421, 422, 423, 424, 425, 426,
-	429, 431, 444, 450, 451, 500, 502, 503, 504, 506, 507, 508, 509, 510, 511, 599
-]
 
 @web_site.route('/')
 def index():
@@ -46,12 +43,13 @@ def generate_user(username):
 
   return render_template('personal_user.html', user=username)
 
-@web_site.route('/page')
-def random_page():
-  return render_template('page.html', code=choice(number_list))
 
 @web_site.route('/database')
 def database_view():
-  return render_template('database_view.html', database = data, color=choice(['red','blue','green']))
+  return render_template('database_view.html', database = data)
+
+@web_site.route('/base')
+def base():
+  return render_template('base.html')
 
 web_site.run(host='0.0.0.0', port=8080, debug=True)
