72ee31b76d7edc3c2e9fd45d3aa2e7516bb76bb1
Harrison Verhine <>
2021-05-24 01:32:33 +0000

    The pull button adds a random character to the user's roster

--- blueprints/pull.py
+++ blueprints/pull.py
@@ -37,5 +37,34 @@ def pickChar():
 @pullBP.route('/pull', methods=['GET','POST'])
 def pull():
   if request.method == 'POST':
-    print(pickChar())
+    player = db['players'][current_user.id]
+
+    replace = 1
+
+    if player['coins'] <= 0:
+      flash("You don't have enough coins.", category='error')
+
+    else:
+
+      #You don't a full roster
+      if player['max_roster'] > len(player['roster']):
+        flash('Who are you going to get...?')
+        new_char = pickChar()
+        db['players'][current_user.id]['roster'].append(new_char)
+
+      #You do have a full roster
+      
+      elif replace is not None and (0 <= replace < len(player['roster'])):
+        old_char = db['players'][current_user.id]['roster'][replace]
+        flash(f"You're trading away someone")
+        new_char = pickChar()
+        db['players'][current_user.id]['roster'][replace] = new_char
+
+      else:
+        flash('Please select a character to replace.')
+        return
+
+      #if successful:
+      db['players'][current_user.id]['coins'] -= 1
+      #await ctx.send(f"Congratulations {player['name']}! You pulled...\n{formatCharacter(new_char)}You have {db['players'][user]['coins']} Koins left.")
   return render_template('pull/pull.html', user = current_user)
