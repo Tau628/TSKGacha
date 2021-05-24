7353cfaea44a3cac95b229bce7e490427d92a81c
Harrison Verhine <>
2021-05-24 20:57:21 +0000

    Creates trade and appends to database

--- blueprints/trades.py
+++ blueprints/trades.py
@@ -13,5 +13,25 @@ def trades():
 
   if request.method == 'POST':
     print(request.form)
+    
+    offer_char = request.form.getlist('offer_character')
+    partner = request.form.get('partner')
+    request_char = request.form.get('request_character').split()
+    offer_coins = int(request.form.get('offer_coins'))
+
+    print(offer_char)
+
+    trade = {
+      'giving': offer_char,
+      'instigator': current_user.id,
+      'partner': partner,
+      'receiving': request_char,
+      'status': 'pending',
+      'coins': offer_coins
+    }
+
+    db['trades'].append(trade)
+
+    print(trade)
 
   return render_template('trades/trades.html', user=current_user, user_roster=roster, other_players=other_players)
