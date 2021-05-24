6680f9109dc11d3e4fd890d2468f2167130747a7
Harrison Verhine <>
2021-05-24 17:44:08 +0000

    Added a display for the a player's roster to their player page.

--- blueprints/players.py
+++ blueprints/players.py
@@ -7,7 +7,9 @@ playersBP = Blueprint('playersBP', __name__, url_prefix='/players')
 @playersBP.route('/<ply_ind>')
 def player_page(ply_ind):
   if current_user.is_authenticated:
-    return render_template('players/player_page.html', user = current_user, player = (ply_ind, database.to_primitive(db['players'][ply_ind])))
+    owned_characters = [(char_id, db['characters'][char_id]) for char_id in db['players'][current_user.id]['roster']]
+
+    return render_template('players/player_page.html', user = current_user, player = (ply_ind, database.to_primitive(db['players'][ply_ind])), owned_characters = owned_characters)
   else:
     return redirect(url_for('home'))
 
