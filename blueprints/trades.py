3acd743f10069327726d358be1745c7357f9abbe
Harrison Verhine <>
2021-05-25 19:51:29 +0000

    Did a lot of work on trades. I don't remember everything

--- blueprints/trades.py
+++ blueprints/trades.py
@@ -38,6 +38,59 @@ def trades():
 
         db['trades'].append(trade)
 
+    elif button.split('-')[0] == 'reject':
+      trade_id = int(button.split('-')[1])
+      flash('Trade rejected.', category='success')
+      db['trades'][trade_id]['status'] = 'rejected'
+
+
+    elif button.split('-')[0] == 'accept':
+      trade_id = int(button.split('-')[1])
+
+      trade = db['trades'][trade_id]
+      if trade['status']!='pending':
+        flash('This is no longer a valid trade.', category='error')
+      
+      elif trade['partner'] != current_user.id:
+        flash('You are not authorized for this trade. Trade deleted.', category='error')
+        db['trades'][trade_id]['status'] = 'errored'
+
+      elif not set(trade['receiving']).issubset(set(db['players'][current_user.id]['roster'])):
+        flash("You don't have the characters for this trade. Trade deleted.", category='error')
+        db['trades'][trade_id]['status'] = 'errored'
+
+      elif not set(trade['giving']).issubset(set(db['players'][trade['instigator']]['roster'])):
+        flash(f"{trade['instigator']} doesn't have the characters for this trade. Trade deleted.", category='error')
+        db['trades'][trade_id]['status'] = 'errored'
+
+      #elif 0 < -trade['coins'] <= db['players'][current_user.id]['coins']:
+      #  flash("You don't have enough coins.", category='error')
+
+      #elif 0 < trade['coins'] <= db['players'][trade['instigator']]['coins']:
+      #  flash(f"{trade['instigator']} don't have enough coins.", category='error')
+
+      else:
+        flash('Trade accepted!', category='success')
+        
+        for char in trade['giving']:
+          print(f"Adding {char} to {current_user.id}")
+          db['players'][current_user.id]['roster'].append(char)
+        for char in trade['receiving']:
+          print(f"Adding {char} to {trade['instigator']}")
+          db['players'][trade['instigator']]['roster'].append(char)
+
+        for char in trade['giving']:
+          print(f"Deleting {char} from {trade['instigator']}")
+          db['players'][trade['instigator']]['roster'].remove(char)
+        for char in trade['receiving']:
+          print(f"Deleting {char} from {current_user.id}")
+          db['players'][current_user.id]['roster'].remove(char)
+
+        db['players'][current_user.id]['coins'] += trade['coins']
+        db['players'][trade['instigator']]['coins'] -= trade['coins']
+
+        db['trades'][trade_id]['status'] = 'accepted'
+
 
   roster = db['players'][current_user.id]['roster']
   roster = [(i, db['characters'][i]) for i in roster]
